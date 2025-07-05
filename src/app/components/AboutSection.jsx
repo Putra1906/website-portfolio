a3e10ecd9e6a2757ce575391644511a16c42b66b
Putra1906 <101515568+Putra1906@users.noreply.github.com>
2025-07-05 14:00:09 +0700

    fix: image

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -189,7 +189,7 @@ const AboutSection = () => {
           animate={inView ? "show" : "hidden"}
         >
           <Image
-            src="/public/images/fix.png"
+            src="/images/about/fix.png"
             width={450}
             height={450}
             alt="About image"
